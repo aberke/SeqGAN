4f219f01275675d1b27cd8ba6562fde6036adcee
aberke <berke.alexandra@gmail.com>
2019-11-17 18:13:18 -0500

    fixed more logging issues

--- sequence_gan.py
+++ sequence_gan.py
@@ -174,8 +174,7 @@ def main():
 
     rollout = ROLLOUT(generator, 0.8)
 
-    print '#########################################################################'
-    write_log('Start Adversarial Training...')
+    write_log(log_fpath, 'Start Adversarial Training...')
     for batch in range(TOTAL_BATCH):
         buff = 'batch %s/%s' % (batch, TOTAL_BATCH)
         write_log(log_fpath, buff)
@@ -189,7 +188,7 @@ def main():
         # Test
         if batch % 5 == 0 or batch == TOTAL_BATCH - 1:
             generate_samples(sess, generator, BATCH_SIZE, generated_num, eval_trajectory_file)
-            write_log(fpath, 'generated some more eval samples...')
+            write_log(log_fpath, 'generated some more eval samples...')
             # likelihood_data_loader.create_batches(eval_file)
             # test_loss = target_loss(sess, target_lstm, likelihood_data_loader)
             # log_progress(log_fpath, batch, test_loss)
@@ -214,7 +213,7 @@ def main():
                     }
                     _ = sess.run(discriminator.train_op, feed)
 
-    write_log(log_fpath, 'DONE')
+    write_log(log_fpath, 'I\'M DONE')
 
 
 if __name__ == '__main__':
